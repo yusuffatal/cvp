9397376534fdd11801524d11a6de4cf7ecf6c71d
yusuffatal <31797247+yusuffatal@users.noreply.github.com>
2019-02-06 22:46:30 +0800

    Update suff.py

--- suff.py
+++ suff.py
@@ -9,7 +9,7 @@ from bs4 import BeautifulSoup
 from googletrans import Translator
 from humanfriendly import format_timespan, format_size, format_number, format_length
 from ffmpy import FFmpeg
-import time, random, sys, json, codecs, threading, glob, re, string, os, requests, six, ast, pytz, urllib, urllib3, urllib.parse, traceback, atexit
+import time, random, sys, json, codecs, threading, glob, re, string, os, requests, six, ast, pytz, urllib, urllib3, urllib.parse, traceback, atexit, pafy
 
 #client = LINE()
 client = LINE("")
